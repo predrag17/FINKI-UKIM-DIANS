6c5f94eaad3a6690f365fbc20a3bd7742698b042
StevkovskaMIA <mia.stevkovska@yahoo.com>
2024-01-24 10:29:20 +0100

    Added

--- Domasna 2/backend/src/main/java/mk/ukim/finki/backend/config/WebSecurityConfig.java
+++ Domasna 2/backend/src/main/java/mk/ukim/finki/backend/config/WebSecurityConfig.java
@@ -8,7 +8,11 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -53,6 +57,17 @@ public class WebSecurityConfig {
                 );
 
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.builder()
+                .username("admin")
+                .password(passwordEncoder.encode("admin"))
+                .authorities("ROLE_ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
     }
 
     @Bean
